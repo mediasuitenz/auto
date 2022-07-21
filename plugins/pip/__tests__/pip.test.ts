3b8c92eb85073643abd335bac5d26d01d70ac3b9
Niall McAndrew <niall@mediasuite.co.nz>
2022-07-21 16:06:13 +1200

    Adds a basic pip plugin.

--- plugins/pip/__tests__/pip.test.ts
+++ plugins/pip/__tests__/pip.test.ts
@@ -1,7 +1,380 @@
-import Auto from '@auto-it/core';
-import Pip from '../src';
+import mockFs from "mock-fs";
+import Pip from "../src";
+import endent from "endent";
+import { makeHooks } from "@auto-it/core/dist/utils/make-hooks";
+import { dummyLog } from "@auto-it/core/dist/utils/logger";
+import { execSync } from "child_process";
+import fs from "fs";
+import { SEMVER } from "@auto-it/core";
 
-describe('Pip Plugin', () => {
-  test('should do something', async () => {
+const logger = dummyLog();
+
+const execSyncSpy = jest.fn();
+jest.mock("child_process");
+// @ts-ignore
+execSync.mockImplementation(execSyncSpy);
+
+const execSpy = jest.fn();
+execSpy.mockReturnValue("");
+
+// @ts-ignore
+jest.mock(
+  "../../../packages/core/dist/utils/exec-promise",
+  () => (...args: any[]) => execSpy(...args)
+);
+
+afterEach(() => {
+  mockFs.restore();
+});
+describe("Pip Plugin", () => {
+  test("throws without a setup config", async () => {
+    expect(() => new Pip({})).toThrow();
+  });
+
+  test("throws when provided a setup config without a name", async () => {
+    mockFs({
+      "setup.cfg": endent`
+        [metadata]
+        version = 1.0.0
+        `,
+    });
+    expect(() => new Pip({})).toThrow();
+  });
+
+  test("doesn't throw any errors with a valid setup config", async () => {
+    mockFs({
+      "setup.cfg": endent`
+        [metadata]
+        name = test-package
+        version = 1.0.0
+      `,
+    });
+    expect(() => new Pip({})).not.toThrow();
+  });
+
+  describe("getPreviousVersion", () => {
+    test("gets previous version from the setup cfg", async () => {
+      mockFs({
+        "setup.cfg": endent`
+        [metadata]
+        name = test-package
+        version = 1.8.99
+      `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      expect(await hooks.getPreviousVersion.promise()).toBe("1.8.99");
+    });
+
+    test("throws if no version is specified in the setup cfg", async () => {
+      mockFs({
+        "setup.cfg": endent`
+        [metadata]
+        name = test-package
+      `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      await expect(hooks.getPreviousVersion.promise()).rejects.toBeInstanceOf(
+        Error
+      );
+    });
+  });
+
+  describe("getAuthor", () => {
+    test("gets author from setup cfg", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          author = Example Author
+          author_email = author@example.com
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      expect(await hooks.getAuthor.promise()).toStrictEqual({
+        name: "Example Author",
+        email: "author@example.com",
+      });
+    });
+    test("gets author from setup cfg using alias", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          author = Example Author
+          author-email = author@example.com
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      expect(await hooks.getAuthor.promise()).toStrictEqual({
+        name: "Example Author",
+        email: "author@example.com",
+      });
+    });
+  });
+
+  describe("getRepository", () => {
+    test("returns if no url found", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      expect(await hooks.getRepository.promise()).toBeUndefined();
+    });
+
+    test("returns if no repo found in url", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          url = http://example.com
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      expect(await hooks.getRepository.promise()).toBeUndefined();
+    });
+
+    test("find repo in homepage", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          url = http://github.com/mediasuitenz/auto
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      expect(await hooks.getRepository.promise()).toStrictEqual({
+        owner: "mediasuitenz",
+        repo: "auto",
+      });
+    });
+  });
+
+  describe("version", () => {
+    test("bump version", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          version = 0.1.0
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+      await hooks.version.promise({ bump: SEMVER.minor });
+
+      expect(fs.readFileSync("setup.cfg", { encoding: "utf-8" })).toBe(endent`
+          [metadata]
+          name = test-package
+          version = 0.2.0
+      `);
+    });
+
+    test("throws with invalid version", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          version = 0.1.nope
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      await expect(
+        hooks.version.promise({ bump: SEMVER.minor })
+      ).rejects.toBeInstanceOf(Error);
+    });
+  });
+
+  describe("publish", () => {
+    beforeEach(() => {
+      execSpy.mockClear();
+    });
+
+    test("calls the python wheel build command", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          version = 0.1.0
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+      await hooks.publish.promise({ bump: SEMVER.minor });
+
+      expect(execSpy).toHaveBeenCalledWith(
+        "python3",
+        ["setup.py", "bdist_wheel", "sdist"]);
+    });
+
+    test("calls the twine upload command", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          version = 0.1.0
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+      await hooks.publish.promise({ bump: SEMVER.minor });
+
+      expect(execSpy).toHaveBeenCalledWith(
+        "python3", [
+          "-m", "twine",
+          "upload",
+          "dist/test-package-0.1.0*",
+          "--verbose"
+        ]);
+    });
+
+    test("calls the twine upload command with a custom repo", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          version = 0.1.0
+        `,
+      });
+
+      const plugin = new Pip({repository: 'example-repo'});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+      await hooks.publish.promise({ bump: SEMVER.minor });
+
+      expect(execSpy).toHaveBeenCalledWith(
+        "python3", [
+          "-m", "twine",
+          "upload",
+          "--repository", "example-repo",
+          "dist/test-package-0.1.0*",
+          "--verbose"
+        ]);
+    });
+  });
+
+  describe("canary", () => {
+    beforeEach(() => {
+      execSpy.mockClear();
+    });
+
+    test("dry-run not execute any commands", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          version = 0.1.0
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      await hooks.canary.promise({
+        bump: SEMVER.minor,
+        canaryIdentifier: "-canary",
+        dryRun: true,
+        quiet: false,
+      });
+
+      expect(execSpy).not.toHaveBeenCalled();
+    });
+
+    test("calls the twine upload command", async () => {
+      mockFs({
+        "setup.cfg": endent`
+          [metadata]
+          name = test-package
+          version = 0.1.0
+        `,
+      });
+
+      const plugin = new Pip({});
+      const hooks = makeHooks();
+
+      plugin.apply({ hooks, logger } as any);
+
+      const result = await hooks.canary.promise({
+        bump: SEMVER.minor,
+        canaryIdentifier: "-canary.1a2b3c4",
+        dryRun: false,
+        quiet: false,
+      });
+
+      expect(result).not.toBe(undefined)
+
+      expect(result.newVersion).toBe(
+        `0.2.0-dev0+1a2b3c4`
+      );
+      expect(result.details).toBe(endent`
+       :sparkles: Test out this PR via:
+
+       \`\`\`bash
+       pip install test-package==0.2.0-dev0+1a2b3c4
+       \`\`\`
+      `);
+
+      expect(execSpy).toHaveBeenCalledWith(
+        "python3", [
+          "-m", "twine",
+          "upload",
+          "dist/test-package-0.2.0.dev0+1a2b3c4*",
+          "--verbose"
+        ]);
+    });
   });
 });
--- plugins/pip/src/index.ts
+++ plugins/pip/src/index.ts
@@ -1,7 +1,14 @@
-import { Auto, IPlugin, validatePluginConfiguration } from '@auto-it/core';
+import { Auto, IPlugin, validatePluginConfiguration } from "@auto-it/core";
 import * as t from "io-ts";
+import glob from "fast-glob";
+import ConfigParser from "configparser";
+import parseGitHubUrl from "parse-github-url";
+import { inc, ReleaseType } from "semver";
+import { readFile, writeFile } from "fs/promises";
+import { execPromise } from "@auto-it/core";
 
 const pluginOptions = t.partial({
+  repository: t.string,
 });
 
 export type IPipPluginOptions = t.TypeOf<typeof pluginOptions>;
@@ -9,23 +16,219 @@ export type IPipPluginOptions = t.TypeOf<typeof pluginOptions>;
 /** Python Pip publishing plugin for auto */
 export default class PipPlugin implements IPlugin {
   /** The name of the plugin */
-  name = 'pip';
+  name = "pip";
 
   /** The options of the plugin */
   readonly options: IPipPluginOptions;
+  readonly config: ConfigParser;
+  readonly packageName: string;
 
   /** Initialize the plugin with it's options */
   constructor(options: IPipPluginOptions) {
+    const setupConfig = glob.sync("setup.cfg")[0];
+
+    if (!setupConfig) {
+      throw new Error("No setup.cfg found");
+    }
+    this.config = new ConfigParser();
+    this.config.read(setupConfig);
+
+    if (!this.config.hasSection("metadata"))
+      throw new Error("No metadata section of setup.cfg");
+
+    const packageName = this.config.get("metadata", "name");
+    if (!packageName)
+      throw new Error("No name in metadata section of setup.cfg");
+
+    this.packageName = packageName;
     this.options = options;
   }
 
-  /** Tap into auto plugin points. */
+  private parseSetupConfig(): {
+    name: string;
+    version: string | undefined;
+    author: { name: string | undefined; email: string | undefined };
+    url: string | undefined;
+  } {
+    const name = this.config.get("metadata", "name");
+    if (!name) throw new Error("No name in metadata section of setup.cfg");
+
+    const version = this.config.get("metadata", "version");
+
+    const author = this.config.get("metadata", "author");
+    const authorEmail =
+      this.config.get("metadata", "author_email") ??
+      this.config.get("metadata", "author-email");
+
+    const url = this.config.get("metadata", "url");
+
+    return { name, version, author: { name: author, email: authorEmail }, url };
+  }
+
   apply(auto: Auto) {
     auto.hooks.validateConfig.tapPromise(this.name, async (name, options) => {
-      // If it's a string thats valid config
-      if (name === this.name && typeof options !== "string") {
+      if (name === this.name || name === `auto-plugin-${this.name}`) {
         return validatePluginConfiguration(this.name, pluginOptions, options);
       }
     });
+
+    auto.hooks.getPreviousVersion.tapPromise(this.name, async () => {
+      const { version } = this.parseSetupConfig();
+
+      if (version === undefined)
+        throw new Error("No version in metadata section of setup.cfg");
+
+      return version;
+    });
+
+    auto.hooks.getAuthor.tapPromise(this.name, async () => {
+      const { author } = this.parseSetupConfig();
+      return author;
+    });
+
+    auto.hooks.getRepository.tapPromise(this.name, async () => {
+      const { url } = this.parseSetupConfig();
+
+      if (!url) return;
+
+      const { owner, name } = parseGitHubUrl(url) || {};
+
+      if (!owner || !name) {
+        return;
+      }
+
+      return {
+        repo: name,
+        owner,
+      };
+    });
+
+    auto.hooks.version.tapPromise(
+      this.name,
+      async ({ bump, dryRun, quiet }) => {
+        const { version, newVersion } = await this.getNewVersion(
+          bump as ReleaseType
+        );
+
+        if (dryRun && newVersion) {
+          if (quiet) {
+            console.log(newVersion);
+          } else {
+            auto.logger.log.info(`Would have published: ${newVersion}`);
+          }
+
+          return;
+        }
+
+        await this.writeNewVersion(version, newVersion);
+      }
+    );
+
+    auto.hooks.canary.tapPromise(
+      this.name,
+      async ({ bump, canaryIdentifier, dryRun, quiet }) => {
+        const { version, newVersion } = await this.getNewVersion(
+          bump as ReleaseType
+        );
+
+        const pipCanaryIdentifier = this.canaryIdentifierToPep404ValidIdentifier(
+          canaryIdentifier
+        );
+        const canaryVersion = `${newVersion}${pipCanaryIdentifier}`;
+
+        if (dryRun) {
+          if (quiet) {
+            console.log(canaryVersion);
+          } else {
+            auto.logger.log.info(`Would have published: ${canaryVersion}`);
+          }
+
+          return;
+        }
+
+        await this.writeNewVersion(version, canaryVersion);
+
+        auto.logger.verbose.info("Running default release command");
+        await execPromise("python3", ["setup.py", "bdist_wheel", "sdist"]);
+
+        // will push the canary gem
+        await execPromise("python3", [
+          "-m",
+          "twine",
+          "upload",
+          ...(this.options.repository
+            ? ["--repository", this.options.repository]
+            : []),
+          `dist/${this.packageName}-${canaryVersion.replace('-', '.')}*`,
+          "--verbose",
+        ]);
+
+        auto.logger.verbose.info("Successfully published canary version");
+
+        return {
+          newVersion: canaryVersion,
+          details: this.makeInstallDetails(this.packageName, canaryVersion),
+        };
+      }
+    );
+
+    auto.hooks.publish.tapPromise(this.name, async () => {
+      const { version } = await this.parseSetupConfig();
+
+      await execPromise("git", [
+        "commit",
+        "-am",
+        `"update version: ${version} [skip ci]"`,
+        "--no-verify",
+      ]);
+
+      auto.logger.verbose.info("Running default release command");
+      await execPromise("python3", ["setup.py", "bdist_wheel", "sdist"]);
+
+      await execPromise("python3", [
+        "-m",
+        "twine",
+        "upload",
+        ...(this.options.repository
+          ? ["--repository", this.options.repository]
+          : []),
+        `dist/${this.packageName}-${version}*`,
+        "--verbose",
+      ]);
+    });
+  }
+  private async getNewVersion(releaseType: ReleaseType) {
+    const { version } = this.parseSetupConfig();
+    if (!version)
+      throw new Error("No version in metadata section of setup.cfg");
+    const newVersion = inc(version, releaseType);
+
+    if (!newVersion)
+      throw new Error(
+        `The version "${version}" parsed from setup.cfg was invalid and could not be incremented.`
+      );
+
+    return { version, newVersion };
+  }
+
+  private async writeNewVersion(version: string, newVersion: string) {
+    const content = await readFile("setup.cfg", { encoding: "utf-8" });
+    await writeFile("setup.cfg", content.replace(version, newVersion));
+  }
+
+  private makeInstallDetails(name: string | undefined, canaryVersion: string) {
+    return [
+      ":sparkles: Test out this PR via:\n",
+      "```bash",
+      `pip install ${name}==${canaryVersion}`,
+      "```",
+    ].join("\n");
+  }
+
+  private canaryIdentifierToPep404ValidIdentifier(identifier: string) {
+    return identifier
+      .replace("-canary", "-dev")
+      .replace(".", "+")
+      .replace('dev+', 'dev0+');
   }
 }
